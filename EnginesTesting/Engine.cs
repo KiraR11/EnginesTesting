c05e5d2c0e908dbe0455059715c04bf0e5567cd8
KiraR11 <123009064+KiraR11@users.noreply.github.com>
2023-11-29 19:03:19 +0700

    Добавление методов расчета пареметров в класс Engine Создание интерфейса тестирования и классов двух тестов

--- EnginesTesting/Engine.cs
+++ EnginesTesting/Engine.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace EnginesTesting
 {
-    internal class Engine
+    public abstract class Engine
     {
+        protected Engine(double tempEnvironment, double superheatTemp, double inertiaMoment, double coefHeatingSpeedOnTorque, double coefHeatingSpeedOnCrankshaft, double coefCoolingSpeedOnTempEngineAndEnvironment)
+        {
+            Temp = tempEnvironment;
+            SuperheatTemp = superheatTemp;
+            CoefHeatingSpeedOnTorque = coefHeatingSpeedOnTorque;
+            CoefHeatingSpeedOnCrankshaft = coefHeatingSpeedOnCrankshaft;
+            CoefCoolingSpeedOnTempEngineAndEnvironment = coefCoolingSpeedOnTempEngineAndEnvironment;
+            InertiaMoment = inertiaMoment;
+        }
+        public double TempEnvironment
+        public double Temp { get; }
+        public double SuperheatTemp { get; }
+        public double CoefHeatingSpeedOnTorque { get; }
+        public double CoefHeatingSpeedOnCrankshaft { get; }
+        public double CoefCoolingSpeedOnTempEngineAndEnvironment { get; }
+        public double InertiaMoment { get; }
+        public double Torque { get; }
+        public double SpeedCrankshaft { get; }
+        public double HeatingSpeed { get { return CalcHeatingSpeed(); } }
+        public double CoolingSpeed { get {return CoolingSpeed(double tempEnvironment); } }
+        public double CalcHeatingSpeed()
+        {
+            double result = Torque * CoefHeatingSpeedOnTorque + SpeedCrankshaft * SpeedCrankshaft * CoefHeatingSpeedOnCrankshaft;
+            return result;
+        }
+
+        public double CoolingSpeed(double tempEnvironment)
+        {
+            double result = CoefCoolingSpeedOnTempEngineAndEnvironment * (tempEnvironment - Temp);
+            return result;
+        }
+
+
     }
 }
